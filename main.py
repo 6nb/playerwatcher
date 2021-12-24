35623f76ec2a3cf18e6b015dc879dafe7dd4f644
bigsnailmonster <enbee419@protonmail.com>
2021-12-24 15:16:03 -0500

    cleaning ig

--- main.py
+++ main.py
@@ -8,44 +8,42 @@ ENTRY_TIME = os.environ['ENTRY_TIME']
 ENTRY_STATUS = os.environ['ENTRY_STATUS']
 ENTRY_PLAYERS = os.environ['ENTRY_PLAYERS']
 ENTRY_MOTD = os.environ['ENTRY_MOTD']
+TIME_FORMAT = '%m-%d-%Y %#I:%M:%S.%f %p'
 
-def playerwatcher():
-    print(f'Checking server stats @ {datetime.now().strftime("%m-%d-%Y %#I:%M:%S.%f %p")}')
-    
-    # Send Request
-    try: res = requests.get(f'https://api.mcsrvstat.us/2/9b9t.com').json()
-    except Exception as err:
-        print('GET failure:', err)
-        return
-
-    if res.get('online') == True:
-        data = {
-            ENTRY_TIME:str(time.time()),
-            ENTRY_STATUS:'Online',
-            ENTRY_PLAYERS:res['players']['online'],
-            ENTRY_MOTD:res['motd']['clean'][0]
-        }
-    else: 
+def main():
+    while True:
+        time.sleep(delay_calc())
+        print(f'Checking server stats @ {datetime.now().strftime(TIME_FORMAT)}')
+        
+        try: request = requests.get(f'https://api.mcsrvstat.us/2/9b9t.com').json()
+        except Exception as error:
+            print('Error Receiving Data', error)
+            return
+        
         data = {
             ENTRY_TIME:str(time.time()),
             ENTRY_STATUS:'Offline',
             ENTRY_PLAYERS:'',
             ENTRY_MOTD:''
         }
-    
-    try: requests.post(URL, data=data)
-    except Exception as err: print('POST failure:', err)
+
+        if request.get('online'):
+            data[ENTRY_STATUS] = 'Online',
+            data[ENTRY_PLAYERS] = request['players']['online'],
+            data[ENTRY_MOTD] = request['motd']['clean'][0]
+        
+        try: requests.post(URL, data=data)
+        except Exception as error:
+            print('Error Sending Data', error)
  
 # Initial Loop Start Delay Calc
 def delay_calc():
     now = datetime.now()
-    print(f'Starting delay calc @ {now.strftime("%m-%d-%Y %#I:%M:%S.%f %p")}')
+    print(f'Starting delay calc @ {now.strftime(TIME_FORMAT)}')
     next_execution = now + (timedelta(minutes=10) - timedelta(minutes=now.minute % 10)) - timedelta(seconds=now.second, microseconds=now.microsecond)
     delay = datetime.timestamp(next_execution) - datetime.timestamp(now)
-    print(f'Calculated next request in {delay}s @ {next_execution.strftime("%m-%d-%Y %#I:%M:%S.%f %p")}')
+    print(f'Calculated next request in {delay}s @ {next_execution.strftime(TIME_FORMAT)}')
     return delay
 
-# Loop
-while True:
-    time.sleep(delay_calc())
-    playerwatcher()
+if __name__ == '__main__':
+    main()
