51c3985959dc0b3991b58e0450f48c045268ede9
bigsnailmonster <enbee419@protonmail.com>
2021-12-29 16:46:41 -0500

    added links; cleaning

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # PlayerWatcher
 
-Simple python script to periodically collect player/motd data from 9b9t.com
+Script that automates player count/motd data collection from 9b9t.com
+
+## [Spreadsheet of Previous Month's Data](https://docs.google.com/spreadsheets/d/1acKc7f4TiEJBWkF2HI4qDgjvYNxUlN_dT5aK2s72pIk/edit?usp=sharing)
+
+Free to use, make nicer charts than mine
+
+## More Links
+- [MOTDS November 2021](https://ghostbin.com/cVfgw)
+- [November Post](https://www.reddit.com/r/9b9t/comments/r6iby1/9b_november_player_count/)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
-import time
 import requests
+import time
 import os
 
 URL = os.environ['URL']
@@ -13,34 +13,27 @@ TIME_FORMAT = '%m-%d-%Y %#I:%M:%S.%f %p'
 def main():
     while True:
         time.sleep(delay_calc())
+
         print(f'Checking server stats @ {datetime.now().strftime(TIME_FORMAT)}')
-        
-        try: request = requests.get(f'https://api.mcsrvstat.us/2/9b9t.com').json()
-        except Exception as error:
-            print('Error Receiving Data', error)
-            return
-        
+
+        request = requests.get(f'https://api.mcsrvstat.us/2/9b9t.com').json()
         data = {
             ENTRY_TIME:str(time.time()),
             ENTRY_STATUS:'Offline',
             ENTRY_PLAYERS:'',
             ENTRY_MOTD:''
         }
-
         if request.get('online'):
             data[ENTRY_STATUS] = 'Online',
             data[ENTRY_PLAYERS] = request['players']['online'],
             data[ENTRY_MOTD] = request['motd']['clean'][0]
-        
-        try: requests.post(URL, data=data)
-        except Exception as error:
-            print('Error Sending Data', error)
+
+        requests.post(URL, data=data)
  
 # Initial Loop Start Delay Calc
 def delay_calc():
     now = datetime.now()
-    print(f'Starting delay calc @ {now.strftime(TIME_FORMAT)}')
-    next_execution = now + (timedelta(minutes=10) - timedelta(minutes=now.minute % 10)) - timedelta(seconds=now.second, microseconds=now.microsecond)
+    next_execution = now + (timedelta(minutes=10) - timedelta(minutes=now.minute % 10, seconds=now.second, microseconds=now.microsecond))
     delay = datetime.timestamp(next_execution) - datetime.timestamp(now)
     print(f'Calculated next request in {delay}s @ {next_execution.strftime(TIME_FORMAT)}')
     return delay
